6d2351a0a1bd24e803e04d43d6599b5241e50279
Erika Santana <erika.araujo12344@gmail.com>
2021-02-09 18:12:53 -0300

    conceitos Vue

---
+++ dataBinding/dataBinding.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hello {{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: 'Érika Santana'
+  })
+}
+</script>
---
+++ singleFileComponents/singleFC.vue
@@ -0,0 +1,15 @@
+<template>
+  <!--Single-File Components
+  -->
+  <h1>Hello World</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass">
+</style>
+
